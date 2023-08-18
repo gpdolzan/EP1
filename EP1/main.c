d50ab3456c67f9ed58a8ad569726e3bad4af2460
Gabriel Dolzan <gabrieldolzan@icloud.com>
2023-08-17 23:17:53 -0300

    Initial Commit

---
+++ EP1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  EP1
+//
+//  Created by Gabriel Dolzan on 17/08/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
